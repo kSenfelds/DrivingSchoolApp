90d5db03490a5627416d6fcd688ce4d7fcf269cc
kSenfelds <kr.senfelds@gmail.com>
2023-05-31 10:14:34 +0300

    update

--- DrivingSchool.Core/School.Services/Validator.cs
+++ DrivingSchool.Core/School.Services/Validator.cs
@@ -3,27 +3,19 @@ using DrivingSchool.Core.Services;
 
 namespace SchoolApi;
 
-public class Validator
+public class Validator: IValidator
 {
     private readonly IEntityService<Student> _service;
+    private const string _theoryTitle = "theory";
+    private const string _drivingTitle = "driving";
     public Validator(IEntityService<Student> service)
     {
         _service = service;
     }
-    public bool IdCannotBeFound(int id)
-    {
-        var student = _service.GetById<Student>(id);
-        if (student == null)
-        {
-            return true;
-        }
-
-        return false;
-    }
-
+    
     public bool InvalidTitle(string mark)
     {
-        if (mark == "theory" || mark == "driving")
+        if (mark == _theoryTitle || mark == _drivingTitle)
         {
             return false;
         }
